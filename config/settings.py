e9b8eae5fb75383e577af49e758494ac08bb2a34
sonrisahy <sonrisa@cap.ocn.ne.jp>
2023-01-21 06:50:17 +0900

    commit 3

--- config/settings.py
+++ config/settings.py
@@ -6,8 +6,7 @@ SECRET_KEY = get_random_secret_key()
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 DEBUG=False
-# ALLOWED_HOSTS=['127.0.0.1' ,'herokuapp.com'] 
-ALLOWED_HOSTS=['*'] 
+ALLOWED_HOSTS=['127.0.0.1' ,'herokuapp.com'] 
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
