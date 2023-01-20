a6d001d5f9625549e69d4292dfad20d85c7efc03
sonrisahy <sonrisa@cap.ocn.ne.jp>
2023-01-20 20:03:17 +0900

    twelevth commit

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path('', top, name='top'),
     path('', include('accounts.urls')),
     path('', include('lifemanage.urls')),
 ]
