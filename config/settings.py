576b322d152e93018faff602177215a52f4e5169
sonrisahy <sonrisa@cap.ocn.ne.jp>
2023-01-21 18:47:31 +0900

    commit 14

--- config/settings.py
+++ config/settings.py
@@ -74,9 +74,7 @@ USE_TZ = True
 AUTH_USER_MODEL = 'accounts.User'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
 )
 # ログイン、ログアウト
 LOGIN_URL = '/login/'
