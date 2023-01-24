8a801e01339e14d2b85be5f162bbe1c102d13f27
sonrisahy <sonrisa@cap.ocn.ne.jp>
2023-01-24 18:42:47 +0900

    commit 2

--- config/settings.py
+++ config/settings.py
@@ -8,7 +8,7 @@ from django.core.management.utils import get_random_secret_key
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
-ALLOWED_HOSTS=['127.0.0.1' ,'herokuapp.com'] 
+ALLOWED_HOSTS=['108.61.223.96' ,'local.host'] 
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -21,7 +21,6 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -51,10 +50,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "name",
-        "USER": "user",
-        "PASSWORD": " ",
-        "HOST": "host",
+        "NAME": "myproject",
+        "USER": "myprojectuser",
+        "PASSWORD": "password",
+        "HOST": "localhost",
         "PORT": " ",
     }
 }
@@ -80,9 +79,10 @@ USE_TZ = True
 AUTH_USER_MODEL = 'accounts.User'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
-)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),
+# )
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # ログイン、ログアウト
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/list/'
@@ -97,25 +97,25 @@ MESSAGE_TAGS = {
     messages.INFO: 'alert alert-info',
     messages.SUCCESS: 'alert alert-success',
 }
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 #デプロイ設定
-DEBUG=False
-try:
-    from .local_settings import *
-except ImportError:
-    pass
+# DEBUG=False
+# try:
+#     from .local_settings import *
+# except ImportError:
+#     pass
 
-if DEBUG:
-    ALLOWED_HOSTS = ['*']
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-else:
-    import environ
-    env = environ.Env()
-    env.read_env(os.path.join(BASE_DIR, '.env'))
+# if DEBUG:
+#     ALLOWED_HOSTS = ['*']
+#     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# else:
+#     import environ
+#     env = environ.Env()
+#     env.read_env(os.path.join(BASE_DIR, '.env'))
      
-    SECRET_KEY = env('SECRET_KEY')
-    ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+#     SECRET_KEY = env('SECRET_KEY')
+#     ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
      
-    STATIC_ROOT = '/usr/share/nginx/html/static'
-    MEDIA_ROOT = '/usr/share/nginx/html/media'
+#     STATIC_ROOT = '/usr/share/nginx/html/static'
+#     MEDIA_ROOT = '/usr/share/nginx/html/media'
      
